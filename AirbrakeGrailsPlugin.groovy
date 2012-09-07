9c1fcdfd050c0b8123fd5f45eee6ea7fca72c3ee
Burt Beckwith <beckwithb@vmware.com>
2012-09-07 13:34:05 -0400

    the developers property is only for extra devs, you're already listed via the author and authorEmail properties

--- AirbrakeGrailsPlugin.groovy
+++ AirbrakeGrailsPlugin.groovy
@@ -10,8 +10,7 @@ class AirbrakeGrailsPlugin {
     def documentation = "http://grails.org/plugin/airbrake"
 
     def license = "APACHE"
-    def developers = [ [ name: "Eric Berry", email: "cavneb@gmail.com" ],
-                       [ name: "Phuong LeCong", email: "phuong@reteltechnologies.com" ]]
+    def developers = [ [ name: "Phuong LeCong", email: "phuong@reteltechnologies.com" ]]
 
     def issueManagement = [ system: "GitHub", url: "https://github.com/cavneb/airbrake-grails/issues" ]
     def scm = [ url: "https://github.com/cavneb/airbrake-grails" ]
