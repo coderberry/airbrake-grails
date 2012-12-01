f2c745199da2e92aefcbe1986921e57dba020a1c
Jon Palmer <jpalmer@care.com>
2012-11-30 23:51:06 -0500

    New config options in Config.groovy. Undo apiKey Config.groovy change.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,6 +1,8 @@
 // configuration for plugin testing - will not be included in the plugin zip
-grails.plugins.airbrake.apiKey = '322311285e3a4df059a5159857c77d18'
-grails.plugins.airbrake.filteredKeys = ['password']
+grails.plugins.airbrake.apiKey = 'YOUR_API_KEY'
+grails.plugins.airbrake.paramsFilteredKeys = ['password']
+grails.plugins.airbrake.sessionFilteredKeys = ['password']
+grails.plugins.airbrake.cgiDataFilteredKeys = ['password']
 grails.plugins.airbrake.includeEventsWithoutExceptions = true
 
 log4j = {
