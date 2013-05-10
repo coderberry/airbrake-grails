80b051eab4d285340b482ceff959b9eb843974f9
Jon Palmer <jpalmer@care.com>
2013-05-10 10:38:54 -0400

    New version of the release plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,7 +12,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:2.1.0", ":rest-client-builder:1.0.2", ":tomcat:$grailsVersion") {
+        build(":release:2.2.1", ":rest-client-builder:1.0.2", ":tomcat:$grailsVersion") {
             export = false
         }
         test(':spock:0.7') {
